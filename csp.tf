c03c279d9090f6d6f14eb3e88419ebb79dac78cc
kavehvaltix <kaveh@valtix.com>
2021-10-26 16:18:02 -0700

    added support for account onboarding

--- csp.tf
+++ csp.tf
@@ -3,9 +3,9 @@ resource "valtix_cloud_account" "aws" {
 	name						= var.valtix_workspaces_csp_name
 	csp_type					= "AWS"
 	aws_credentials_type		= "AWS_IAM_ROLE"
-	aws_iam_role				= var.valtix_aws_controller_role_arn
-	aws_account_number			= var.valtix_aws_account
-	aws_iam_role_external_id	= var.valtix_aws_external_id
+	aws_iam_role				= var.valtix_ws_aws_controller_role_arn
+	aws_account_number			= var.valtix_ws_aws_account
+	aws_iam_role_external_id	= var.valtix_ws_aws_external_id
 	inventory_monitoring {
     	regions					= var.valtix_csp_regions
     	refresh_interval		= 10
