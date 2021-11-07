8650379ba5fa4ae4222f79eb4fcfe52c8342c465
kavehvaltix <kaveh@valtix.com>
2021-11-07 09:10:10 -0800

    updated to add transit vpc and VM

--- service_vpc.tf
+++ service_vpc.tf
@@ -20,10 +20,31 @@ resource "valtix_spoke_vpc" "valtix_workspaces_spoke" {
 }
 
 #updating spoke route table
-resource "aws_route" "route" {
+resource "aws_route" "workspaces_route" {
   provider               = aws.aws-workspaces
   count                  = length(var.spoke_route_table_ids)
   route_table_id         = var.spoke_route_table_ids[count.index]
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = aws_ec2_transit_gateway.valtix_tgw.id
+  depends_on = [
+    time_sleep.wait_for_tgw_sharing
+  ]
+}
+
+#Adding Transit VPC to Valtix
+resource "valtix_spoke_vpc" "valtix_tansit_vpc_spoke" {
+  service_vpc_id             = valtix_service_vpc.valtix_svpc.id
+  spoke_vpc_id               = transit_vpc.vpc_id
+  spoke_vpc_csp_account_name = var.valtix_csp_name
+  spoke_vpc_region           = var.valtix_aws_region
+  depends_on = [
+    aws_ram_resource_share.shared_valtix_tgw, aws_ram_resource_association.tgw_sharing,valtix_service_vpc.valtix_svpc
+  ]
+}
+
+#updating Transit VPC route table 
+resource "aws_route" "transit_vpc_route" {
+  route_table_id         = aws_vpc.vpc.default_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id     = aws_ec2_transit_gateway.valtix_tgw.id
   depends_on = [
